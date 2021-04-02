0ef8a6e5c684962ef6f96cee6c289e96d7fd0e16
muhamadariefr <m.arief.r54321@gmail.com>
2021-04-02 08:48:41 +0700

    submission dicoding academy

---
+++ js/navigasi.js
@@ -0,0 +1,8 @@
+/* NAVIGASI MENU */
+function openNav() {
+    document.getElementById("menu").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("menu").style.width = "0";
+}
